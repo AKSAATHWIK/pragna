fae7e8ac4c887156747dac6c86d37d959cc8cbdd
AKSAATHWIK <81167098+AKSAATHWIK@users.noreply.github.com>
2022-04-06 13:13:13 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,27 +1,36 @@
 <?php
-$name= $_POST['name'];
-$visitor_email= $_POST['email'];
-$subject= $_POST['subject'];
-$message= $_POST['message'];
 
-$email_form = 'pragna.co.in';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-$email_subject = 'New Form Submission';
+$mailheader = "From:".$name."<".$email.">\r\n";
 
-$email_body = "User Name: $name.\n". 
-               "User Email: $visitor_email.\n".
-               "Subject: $subject.\n".
-               "User Message: $message .\n";
+$recipient = "pragnalegalservices@gmail.com";
 
-$to = 'pragnalegalservices@gmail.com';
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
-$headers = "From: $email_from \r\n";
+echo'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
+        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
+        
+    </div>
+</body>
+</html>
+';
 
-$headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: contact.html");
-
-
-?>               
+?>
